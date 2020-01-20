25a57bc40c3d1e833d587863453d9929dcc2c89a
Dominik Trusinski <dominik.trusinski@gmail.com>
2020-01-20 14:10:29 +0100

    Log using proper logger name condition. Append to log files instead of creating new

--- src/main/java/filters/AkkaFilter.java
+++ src/main/java/filters/AkkaFilter.java
@@ -8,7 +8,7 @@ public class AkkaFilter extends Filter<ILoggingEvent> {
 
     @Override
     public FilterReply decide(ILoggingEvent event) {
-        boolean isSag = event.getMarker().contains("Sag");
+        boolean isSag = event.getLoggerName().contains("sag");
         if (!isSag) {
             return FilterReply.ACCEPT;
         } else {
